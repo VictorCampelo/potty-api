cced5c3cabf083519f5455d514a1ed906e8f8398
Rodrigo Brito <spensmusic@gmail.com>
2021-09-06 09:56:46 -0300

    feat: import StoreRepository and export StoresService

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StoresService } from './stores.service';
 import { StoresController } from './stores.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoreRepository } from './stores.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StoreRepository])],
   controllers: [StoresController],
   providers: [StoresService],
+  exports: [StoresService],
 })
 export class StoresModule {}
