7ce4c9ac17ffd29af49725568cee2b735b5319eb
Rodrigo Brito <spensmusic@gmail.com>
2022-01-23 12:45:14 -0300

    wip(plan): unit tests

---
+++ src/plans/__tests__/util/util.ts
@@ -0,0 +1,11 @@
+import { Plan } from 'src/plans/entities/plan.entity';
+
+export default class Util {
+  static giveMeAValidPlan(): Plan {
+    const plan = new Plan();
+    plan.name = 'Plano Teste 1.0';
+    plan.qtd_products = 100;
+    plan.id = '1';
+    return plan;
+  }
+}
