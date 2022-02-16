c9fd61d9ccda262104f1c99fb90572ad6df138dc
Rodrigo Brito <spensmusic@gmail.com>
2022-02-16 15:30:18 -0300

    feat(stores): payment method integration :sparkles:

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,6 +1,7 @@
 interface IProducts {
   productId: string;
   amount: number;
+  paymentMethod: string;
   parcels?: number;
 }
 interface IOrder {
@@ -18,4 +19,5 @@ export interface IProductsToListMsg {
   amount: number;
   title: string;
   parcels?: number;
+  paymentMethod: string;
 }
